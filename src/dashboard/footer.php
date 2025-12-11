eba1f65ef0c136a195d59641ce9b8daf0ed8e6e8
MarquesLourenco <lourencomiguelmarques1@gmail.com>
2025-12-11 11:53:17 +0000

    reciclagem de problemas

--- src/dashboard/footer.php
+++
@@ -1,9 +0,0 @@
-    </main>
-    <footer>
-        <div class="container">
-            <p>&copy; <?= date('Y') ?> LouJoa - Leilões em Tempo Real. Todos os direitos reservados.</p>
-        </div>
-    </footer>
-    <script src="assets/js/main.js"></script>
-</body>
-</html> 
